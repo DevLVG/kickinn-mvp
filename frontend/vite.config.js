dca33be94154c95727cd9c4344d047b0684c2ae7
Marcello Piccardo <marcellopiccardo@gmail.com>
2025-04-20 14:01:02 +0400

    Fix Vercel build setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    sourcemap: true,
+    emptyOutDir: true
   },
   server: {
     port: 3000
